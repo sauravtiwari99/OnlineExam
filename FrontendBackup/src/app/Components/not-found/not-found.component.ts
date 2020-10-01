6b7d3c4dad3e06d1829a18c082b3c942405ac35c
Ajay Upadhyaya <ajayu712@gmail.com>
2020-10-01 17:56:42 +0530

    Frontend Update 21
    Invalid Route Page Completed

--- FrontendBackup/src/app/Components/not-found/not-found.component.ts
+++ FrontendBackup/src/app/Components/not-found/not-found.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _route:Router) { }
 
   ngOnInit(): void {
+  }
+  loginRoute(){
+    this._route.navigate([""]);
   }
 
 }
